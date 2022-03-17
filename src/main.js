20e98e3b077c2ff70bc8ab63996e70c086f78d82
Andrés País <andrespais61@gmail.com>
2022-03-16 23:58:23 -0300

    ejercicio terminado

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const carrito = [];
+
+const impresiones3D = [
+    {id: 1, titulo: 'Darwin', precio: 2000},
+    {id: 2, titulo: 'Turing', precio: 2000},
+    {id: 3, titulo: 'Newton', precio: 2000},
+    {id: 4, titulo: 'Einstein', precio: 2000},
+]
+
+function agregarAlCarrito(impresiones3D) {
+    document.write(impresiones3D[0])
+}
+
